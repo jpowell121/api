413c590dc5880f9b645ffcd5943460e1720db3c7
jpowell121 <jpowell@joelpowell.com>
2019-12-05 23:29:29 -0500

    step 4.6 Tell the user no products found

--- api/product/read.php
+++ api/product/read.php
@@ -53,4 +53,13 @@ if($num>0){
     echo json_encode($products_arr);
 }
 
-// no products found will be here
+else{
+
+    // set response code - 404 Not found
+    http_response_code(404);
+
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "No products found.")
+    );
+}
